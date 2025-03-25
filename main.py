fd554ced732334b29718406e7f568d179477c66e
C0DER4lif3 <topar@students.dsbn.org>
2025-03-25 12:32:15 -0400

    done

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#rylan
+#march 20
+#starwars magazine quiz 
+#this is a quiz that asks starwars based questions and totals up points to get a score out of 200
 
+import random
+#create list 
+
+numbers =[]
+for i in range (100):
+    num=random.randint(0,100)
+    numbers.append(num)
+
+print (sum (numbers)/100)
